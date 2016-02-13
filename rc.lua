73a65a7f4d007ab158ad9421e9b52bd7a9188cf0
Fred-si <fred-si@zaclys.net>
2016-02-13 22:38:24 +0100

    Passe la variable layout de local à globale

--- rc.lua
+++ rc.lua
@@ -68,7 +68,7 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 if hostname == "lysa" then
-	local layouts = {
+	 layouts = {
 		awful.layout.suit.max,
 		awful.layout.suit.spiral.dwindle,
 		awful.layout.suit.tile,
@@ -84,7 +84,7 @@ if hostname == "lysa" then
 	}
 
 else
-	local layouts = {
+	layouts = {
 		awful.layout.suit.spiral.dwindle,
 		awful.layout.suit.tile,
 		awful.layout.suit.tile.bottom,
@@ -100,6 +100,22 @@ else
 	}
 
 end
+
+	 --layouts = {
+		--awful.layout.suit.max,
+		--awful.layout.suit.spiral.dwindle,
+		--awful.layout.suit.tile,
+		--awful.layout.suit.tile.bottom,
+		--awful.layout.suit.fair.horizontal,
+		--awful.layout.suit.max.fullscreen,
+		--awful.layout.suit.floating,
+		--awful.layout.suit.magnifier,
+	 --   awful.layout.suit.tile.left,
+	--    awful.layout.suit.tile.top,
+	--    awful.layout.suit.fair,
+	--    awful.layout.suit.spiral,
+	--}
+
 
 -- }}}
 
