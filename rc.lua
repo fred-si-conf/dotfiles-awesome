59cf9903b82db3ca0c88106977e2775a23146005
Fred-si <fred-si@zaclys.net>
2018-02-24 13:24:01 +0100

    Supprime les raccourcis sur la molette de la souris
    Ces raccourcis faisaient changer de tag lorsqu'on tournait la molette de
    la souris sur le bureau... inutile pour moi.

--- rc.lua
+++ rc.lua
@@ -359,9 +359,9 @@
 
 -- Mouse bindings
 	root.buttons(awful.util.table.join(
-		awful.button({ }, 3, function () mymainmenu:toggle() end),
-		awful.button({ }, 4, awful.tag.viewnext),
-		awful.button({ }, 5, awful.tag.viewprev)
+		--awful.button({ }, 4, awful.tag.viewnext),
+		--awful.button({ }, 5, awful.tag.viewprev),
+		awful.button({ }, 3, function () mymainmenu:toggle() end)
 	))
 
 -- Key bindings 
