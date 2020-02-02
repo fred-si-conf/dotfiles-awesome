2711961c6812f0e6fa01fd92afc7ed55061cbf97
Fred-si <fred-si@zaclys.net>
2020-02-02 10:45:33 +0100

    Ajoute le widget cpu

--- rc.lua
+++ rc.lua
@@ -226,6 +226,13 @@ if hostname == "lysa" then
     vicious.register(batterywidget, vicious.widgets.bat, "$2", 120, "BATC")
 end
 
+cpuwidget = wibox.widget.graph()
+cpuwidget:set_width(30)
+cpuwidget:set_background_color"#494B4F"
+cpuwidget:set_color{type = "linear", from = {0, 0}, to = {50, 0},
+                    stops = {{0, "#FF5656"}, {0.5, "#88A175"}, {1, "#AECF96"}}}
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1", 1)
+
 -- Create a wibox for each screen and add it
 local taglist_buttons, tasklist_buttons, buttons
 buttons = {one = awful.button({}, 1, function(t) t:view_only() end),
