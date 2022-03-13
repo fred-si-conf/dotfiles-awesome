2e8877c010524d40dc0322fc62af9b107b030f29
Fred-si <fred-si@zaclys.net>
2022-03-13 15:38:43 +0100

    Feat: Créer une fonction d’aide pour dump les propriétés d’un client

--- utils/dump_table.lua
+++ utils/dump_table.lua
@@ -1,16 +1,45 @@
 -- Une simple fonction pour afficher le contenue d’une table en phase
 -- de debogage
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = k end
-         s = s .. k .. ' = ' .. dump(v) .. ', '
-      end
-      return s .. '}'
+function dump(o, indent_level)
+   t = type(o)
+   if t == 'table' then
+      return dump_table(o, indent_level)
+   elseif t == 'client' then
+      return dump_client_properties(o)
    else
       return tostring(o)
    end
+end
+
+function dump_table(table, indent_level, indent_size)
+   indent_level = indent_level or 1
+   local base_indent = string.rep(' ', indent_size or 4)
+   local indent = string.rep(base_indent, indent_level)
+
+   local s = '{\n'
+   for k,v in pairs(table) do
+      s = s .. indent .. k .. ' = ' .. dump(v, indent_level + 1) .. ',\n'
+   end
+   return s .. string.rep(base_indent, indent_level - 1) .. '}'
+end
+
+function dump_client_properties(client)
+   props_to_dump = {
+      'window',
+      'leader_window',
+      'name',
+      'class',
+      'instance',
+      'type',
+      'role',
+   }
+
+   local t = {}
+   for _, prop in ipairs(props_to_dump) do
+      t[prop] = client[prop]
+   end
+
+   return dump_table(t)
 end
 
 return dump
