8a42889cc493a459b41c17feefb25d52946d5336
Fred-si <fred-si@zaclys.net>
2022-03-13 18:01:14 +0100

    Feat: Ajoute une entrée restart au menu

--- status_bar/menu.lua
+++ status_bar/menu.lua
@@ -1,7 +1,10 @@
 local awful = require("awful")
 local hotkeys_popup = require("awful.hotkeys_popup").widget
 
-local menu_items = {{"hotkeys", function() return false, hotkeys_popup.show_help end}}
+local menu_items = {
+    {"hotkeys", function() return false, hotkeys_popup.show_help end},
+    {"restart", awesome.restart}
+}
 local main_menu = awful.menu({items = menu_items})
 
 return main_menu
