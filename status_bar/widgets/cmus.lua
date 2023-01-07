f9a116bacc519afa67cb8305426c10e4d3e54bde
Fred-si <fred-si@zaclys.net>
2023-01-07 23:05:21 +0100

    StatusBar: utilise le widget cmus intégré à vicious

--- status_bar/widgets/cmus.lua
+++ status_bar/widgets/cmus.lua
@@ -1,8 +1,6 @@
 local vicious = require("vicious")
 local helpers = require("vicious.helpers")
 
-local get_state = require(select(1, ...) .. ".get_state")
-
 text_format = "$status: $artist - $title"
 
 local function to_string(args)
@@ -11,7 +9,7 @@ end
 
 local widget = require("wibox").widget.textbox() 
 
-vicious.register(widget, function() return to_string(get_state()) end, "$1", 1)
+vicious.register(widget, vicious.widgets.cmus, "${status}: ${artist} - ${title}", 1)
 
 return widget
 
